a85841d4bb1d2cfe415fd7efaa87fe709ba2ed66
osreboot <calvin.16.weaver@gmail.com>
2018-07-05 17:58:39 -0700

    dsfs

--- src/com/hyprgloo/mazegame/client/Game.java
+++ src/com/hyprgloo/mazegame/client/Game.java
@@ -10,26 +10,27 @@ import com.hyprgloo.mazegame.KC;
 import com.osreboot.ridhvl.HvlCoord2D;
 
 public class Game {
-	
+
 	public Player player;
 	public ArrayList<Block> blocks;
 
 	public Game() {
 		player = new Player(500, 500);
 	}
-	
+
 	public void update(float delta) {
 		player.update(delta);
 		player.draw(delta);
 		MainClient.getNewestInstance().getClient().setValue(KC.key_UIDLocation(MainClient.getNewestInstance().getUIDK()), player.pos, false);
-		
-		ArrayList<HvlCoord2D> coords = MainClient.getNewestInstance().getClient().getTable().<ArrayList<HvlCoord2D>>getSValue(KC.key_UIDEnemies(MainClient.getNewestInstance().getUIDK()));
-		
-		for(int i = 0; i < coords.size(); i++) {
-			hvlDrawQuad(coords.get(i).x, coords.get(i).y, 50, 50, Color.red);
+
+		if(MainClient.getNewestInstance().getClient().getTable().getPopulation(KC.key_UIDEnemies(MainClient.getNewestInstance().getUIDK())) > 0){
+			ArrayList<HvlCoord2D> coords = MainClient.getNewestInstance().getClient().getTable().<ArrayList<HvlCoord2D>>getSValue(KC.key_UIDEnemies(MainClient.getNewestInstance().getUIDK()));
+			for(int i = 0; i < coords.size(); i++) {
+				hvlDrawQuad(coords.get(i).x, coords.get(i).y, 50, 50, Color.red);
+			}
 		}
-		
+
 	}
 
-	
+
 }
