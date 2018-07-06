9b259c93a4d15a44fff423d6155d584dc4a3b53f
Zack <Zack@DESKTOP-T4SQJO2>
2018-07-05 19:59:22 -0500

    dadwsdw

--- src/com/hyprgloo/mazegame/client/Game.java
+++ src/com/hyprgloo/mazegame/client/Game.java
@@ -1,8 +1,13 @@
 package com.hyprgloo.mazegame.client;
 
+import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.hvlDrawQuad;
+
 import java.util.ArrayList;
 
+import org.newdawn.slick.Color;
+
 import com.hyprgloo.mazegame.KC;
+import com.osreboot.ridhvl.HvlCoord2D;
 
 public class Game {
 	
@@ -17,6 +22,12 @@ public class Game {
 		player.update(delta);
 		player.draw(delta);
 		MainClient.getNewestInstance().getClient().setValue(KC.key_UIDLocation(MainClient.getNewestInstance().getUIDK()), player.pos, false);
+		
+		ArrayList<HvlCoord2D> coords = MainClient.getNewestInstance().getClient().getTable().<ArrayList<HvlCoord2D>>getSValue(KC.key_UIDEnemies(MainClient.getNewestInstance().getUIDK()));
+		
+		for(int i = 0; i < coords.size(); i++) {
+			hvlDrawQuad(coords.get(i).x, coords.get(i).y, 50, 50, Color.red);
+		}
 		
 	}
 
